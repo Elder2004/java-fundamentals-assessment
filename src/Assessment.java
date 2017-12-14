3f9b70802fabcb540e444fd72931168627c5101c
Tony Westbrook <tonywestbrook58@mail.com>
2017-12-14 15:40:28 -0600

    first commit

---
+++ src/Assessment.java
@@ -0,0 +1,48 @@
+public class Assessment {
+
+
+public static double square;
+public static double sum;
+public static double avg;
+
+    public static double main(String[] args) {
+
+       int[] no = new int[0];
+        for (int i = 0; i < no.length; i++) {
+            int x = no[i];
+            if (x % 2 == 0) continue;
+            int y = (int) Math.sqrt (x);
+            if (x == y * y) {
+                System.out.println (x);
+            }
+        }
+
+        int num = 321;
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+            System.out.println(sum);
+
+
+            public static double calcAverage(); {
+                int sum = 0;
+                int people;
+                for (int i = 0; i < people.length; i++)
+                {
+                    sum = sum + people[i];
+                }
+                double average = sum / (double)people;
+                return average;
+            }
+
+        }
+
+
+
+
+    }
+
+
+};
+
